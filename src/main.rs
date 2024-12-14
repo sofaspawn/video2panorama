9ec41bb14ba256c8c286a7c58caab1c3065870fa
sofaspawn <h.m1nus.work@gmail.com>
2024-12-14 18:12:05 +0530

    starting all over

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use ffmpeg_next as ffmpeg;
-
 fn main() {
-    let filename = "test.mp4";
-    let frames = create_frames(filename);
+    println!("Hello");
 }
-
-fn create_frames(filename: &str) {}
