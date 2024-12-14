251f27d11db78f2028ecc1c7db56dbc714110ecb
sofaspawn <h.m1nus.work@gmail.com>
2024-12-14 18:48:22 +0530

    frustrated

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::process::Command;
 
 fn main() {
-    let mut hello = Command::new("echo");
-    println!(
-        "{:?}",
-        String::from_utf8(hello.arg("something").output().unwrap().stdout).unwrap()
-    );
+    let filename = "/home/sofaspawn/progs/video2panorama/ftest.mp4";
+    // ffmpeg -i input.mp4 -vf "select=gt(scene\,0.01),setpts=N/FRAME_RATE/TB" -vsync vfr frame_%04d.bmp
+    let mut ff = Command::new("ffmpeg").arg("-i").arg(filename);
+    let _ = ff.status().unwrap();
+    let mut ls = Command::new("ls");
+    ls.current_dir("frames/")
+        .status()
+        .expect("process failed to execute");
 }
